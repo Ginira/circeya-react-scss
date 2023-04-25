e7805f012fc96864336060c3c2f5cec6bd8d1f6c
Ginira <clo3er@gmail.com>
2023-04-25 14:03:24 +0300

    block1 layout done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import logoJulia from './img/julia.svg'
+import logoCirceya from './img/logoCirceya.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="menu-header">
+          <img className="logo" src={logoCirceya} alt="circeya"/>
+          <div className="mobili-number">+7 (495) 495-49-54</div>
+        </div>
       </header>
+      <div className="main-container">
+          <div className="container"> 
+            <h1 className="title-block">ut aliquip ex ea commodo consequat</h1>
+              <img className="logo-julia" src={logoJulia} alt="logoJulia"/>
+                <p className="title-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <p className="text">Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            
+          </div>
+      </div>
     </div>
   );
 }
